42df5231e0b6b357d30d3e6da6523c35ca56067d
iliaselboazzati <ilias-elboazzati@hotmail.com>
2021-06-01 15:36:48 +0200

    suite Exo12 evenListeners

--- main2.js
+++ main2.js
@@ -1,20 +1,21 @@
 // ## Exo 01 
 // > Créez un bouton en JS avec une valeur "Je suis un bouton"
 // > Au clique la valeur du bouton devient "j'ai été cliqué"
-let myBody = document.querySelector('body');
-let btn = document.createElement('button');
+
+let myBody = document.querySelector("body");
+let btn = document.createElement("button");
 btn.textContent = "Je suis un bouton";
 myBody.appendChild(btn);
 
-btn.addEventListener("click", () =>{
+btn.addEventListener("click", () => {
     btn.textContent = "J'ai été cliqué";
 });
+
+
 // ## Exo 02
 // > Créer un 2eme button
-// > Au clique la valeur du button devient 
-// "j'ai été cliqué x fois" le x est le nombre de clique effectué.
-
-let btn2 = document.createElement('button');
+// > Au clique la valeur du button devient "j'ai été cliqué x fois" le x est le nombre de clique effectué.
+let btn2 = document.createElement("button");
 btn2.textContent = "Je suis un bouton";
 myBody.appendChild(btn2);
 let x = 0;
@@ -25,8 +26,7 @@ btn2.addEventListener("click", () => {
 });
 
 // ## Exo 03
-// > Créer un button a qui vous assignerez l'écouteur d'évenement click qui rajoutera un titre
-//  dans votre page web
+// > Créer un button a qui vous assignerez l'écouteur d'évenement click qui rajoutera un titre dans votre page web
 let monB = document.createElement("button");
 monB.textContent = "Ajout d'un titre";
 document.body.appendChild(monB);
@@ -38,12 +38,8 @@ monB.addEventListener('click', () => {
     document.body.appendChild(monH);
 });
 
-
-
 // ## Exo 04
-// > Créer un buttton, assigner l'écouteur d'évenement click.
-//  Ce bouton rajoutera un button dans votre page web avec comme valeur Exo 04
-
+// > Créer un buttton, assigner l'écouteur d'évenement click. Ce bouton rajoutera un button dans votre page web avec comme valeur Exo 04
 let monB2 = document.createElement("button");
 monB2.textContent = "Ajout d'un bouton";
 document.body.appendChild(monB2);
@@ -59,7 +55,6 @@ monB2.addEventListener('click', () => {
 // > Créez un paragraphe avec du texte
 // > Créez un button "modifier la couleur du paragraphe"
 // > Au clique ça changera la couleur du paragraphe
-
 let monP = document.createElement("p");
 monP.textContent = "je suis un paragraphe";
 document.body.appendChild(monP);
@@ -78,11 +73,9 @@ monB4.addEventListener("click", () => {
 // > Créer un button qui crée un h2 avec une valeur "Je lance une function"
 // > Rajouter le titre sur la page web
 // > Etape 2 :
-// > Cette function doit également rajouter un boutton avec comme valeur 
-// "Modifier le titre Exo 06"
+// > Cette function doit également rajouter un boutton avec comme valeur "Modifier le titre Exo 06"
 // > Etape 3 :
-// > le bouton "Modifier le titre" lance une function qui modifie le texte
-//  en lui mettant sa couleur en rouge.
+// > le bouton "Modifier le titre" lance une function qui modifie le texte en lui mettant sa couleur en rouge.
 
 let monB5 = document.createElement("button");
 monB5.textContent = "Button";
@@ -102,5 +95,3 @@ monB5.addEventListener('click', () => {
 btnEnPlus.addEventListener("click", () => {
     monH2.style.color = "red";
 });
-
-
